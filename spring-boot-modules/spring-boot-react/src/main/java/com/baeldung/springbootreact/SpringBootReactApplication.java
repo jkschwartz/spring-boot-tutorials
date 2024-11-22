7f3ce746eed12cc4c169ec0dcf8ad5ff158f4afc
Jordan Schwartz <jordan@citypt.com>
2024-11-22 14:24:49 -0600

    Adding spring actuator

--- spring-boot-modules/spring-boot-react/src/main/java/com/baeldung/springbootreact/SpringBootReactApplication.java
+++ spring-boot-modules/spring-boot-react/src/main/java/com/baeldung/springbootreact/SpringBootReactApplication.java
@@ -2,10 +2,16 @@ package com.baeldung.springbootreact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootReactApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootReactApplication.class, args);
+        ApplicationContext apc = SpringApplication.run(SpringBootReactApplication.class, args);
+        for (String s : apc.getBeanDefinitionNames()) {
+            System.out.println(s);
+        }
+        
     }
+
 }
